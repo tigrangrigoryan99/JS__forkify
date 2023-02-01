92a3637143c7efd42b47eb0d07d710703aaba74f
Tigran <tigran.grigoryan.2021@inbox.ru>
2023-02-02 00:24:50 +0400

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -266,10 +266,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcom to the application!');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -278,6 +274,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerCilck(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
